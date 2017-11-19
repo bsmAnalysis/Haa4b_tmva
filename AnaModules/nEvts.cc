f7424648871a9e7f748d6135da40c3dd355f8c59
hua wei <hua.wei@cern.ch>
2017-11-19 01:47:11 -0600

    good, test done

--- AnaModules/nEvts.cc
+++ AnaModules/nEvts.cc
@@ -29,12 +29,9 @@ int main(int argc, char* argv[])
   //root://eoscms.cern.ch//eos/cms/store/user/georgia/results_2017_09_21/WWZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/crab_MC13TeV_WWZ_2016_0/170921_183914/0000/analysis_1.root
   //root://eoscms.cern.ch//eos/cms/store/user/georgia/results_2017_09_21/WZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/crab_MC13TeV_WZZ_2016_0/170921_183833/0000/analysis_1.root
   //root://eoscms.cern.ch//eos/cms/store/user/georgia/results_2017_09_21/ZZZ_TuneCUETP8M1_13TeV-amcatnlo-pythia8/crab_MC13TeV_ZZZ_2016_0/170921_183749/0000/analysis_1.root
-
   //root://eoscms.cern.ch//eos/cms/store/user/georgia/results_2017_09_21/TTJets_TuneCUETP8M2T4_13TeV-amcatnloFXFX-pythia8/crab_MC13TeV_TTJets_2016_0/171005_184952/0000/analysis_1.root
   
   std::string input_str(argv[1]);
-  std::ifstream inputFile( input_str );
-  std::cout << "File list name: " << input_str << std::endl;
   /* 
   //set input for handler
   TFile *inputFile = TFile::Open( input_str.c_str() );
@@ -45,7 +42,11 @@ int main(int argc, char* argv[])
   std::cout << "Read from histo: " << std::endl;
   std::cout << "nTot: " << nevtH->GetBinContent(1) << std::endl;
   if( posH && negH ) std::cout << posH->GetBinContent(1) << " - " << negH->GetBinContent(1) << std::endl;
+  inputFile->Close();
   */
+  
+  std::ifstream inputFile( input_str );
+  std::cout << "File list name: " << input_str << std::endl;
   std::vector<std::string> fnames;
   for( std::string line; getline( inputFile, line ); )
   {
@@ -72,7 +73,7 @@ int main(int argc, char* argv[])
   std::cout << "nTot: " << nTot << std::endl;
   std::cout << "nPos: " << nPos << ", nNeg: " << nNeg << std::endl;
   std::cout << nPos << " - " << nNeg << std::endl;
-
+  
   /*
   TChain *chain = new TChain("mainNtuplizer/data");
   for (int i = 0; i < fnames.size(); i++)
