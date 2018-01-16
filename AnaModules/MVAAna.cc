2a8dd6ce9403badd9c67c6bd1e280f1b3bee07b0
weihua19900704 <hua.wei@cern.ch>
2018-01-16 07:08:22 +0100

    kf fix in ana phrase

--- AnaModules/MVAAna.cc
+++ AnaModules/MVAAna.cc
@@ -172,7 +172,7 @@ int main(int argc, char* argv[])
     myBGSampleWeight.QCDSampleInfo_push_back( "_TTJets_dl_"           ,            831.76*TTbar_DiLept_BR, 30444678 - 0, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     //myBGSampleWeight.QCDSampleInfo_push_back( "_TTJets_dl_ext1_"      ,            831.76*TTbar_DiLept_BR, 30444678 - 0, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     //myBGSampleWeight.QCDSampleInfo_push_back( "_WJets_"                ,           61526.7, 20308674 - 3811645, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
-    myBGSampleWeight.QCDSampleInfo_push_back( "_WJets_"                ,           61526.7, 86731806 - 0, LumiScale, 1.21, "FileList/MVABGMCFileList.txt", TrainMode );
+    myBGSampleWeight.QCDSampleInfo_push_back( "_WJets_"                ,           61526.7, 86731806 - 0, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode ); //kf == 1, since 61526.7 is already NNLO cross section
     //myBGSampleWeight.QCDSampleInfo_push_back( "_DYJetsToLL_10to50_"   ,             18610, 26709558 - 4211038, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     myBGSampleWeight.QCDSampleInfo_push_back( "_DYJetsToLL_10to50_"   ,             18610, 61593515 - 9708472, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     myBGSampleWeight.QCDSampleInfo_push_back( "_DYJetsToLL_50toInf_"  ,            5765.4, 101918226 - 20137162, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
@@ -199,6 +199,7 @@ int main(int argc, char* argv[])
     myBGSampleWeight.QCDSampleInfo_push_back( "_QCD_Pt300ToInf_EMEnr_",         9000*0.15,  7373633 - 0, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     myBGSampleWeight.QCDSampleInfo_push_back( "_QCD_Pt20ToInf_MuEnr_" , 720648000*0.00042, 22094081 - 0, LumiScale, 1, "FileList/MVABGMCFileList.txt", TrainMode );
     myBGSampleWeight.GenLatexTable();
+    return 0;
     //return -1;
     std::cout << "Processing background MC samples..." << std::endl;
     for(std::vector<QCDSampleInfo>::iterator iter_SampleInfos = myBGSampleWeight.QCDSampleInfos.begin(); iter_SampleInfos != myBGSampleWeight.QCDSampleInfos.end(); iter_SampleInfos++)
